d84c03f36859e19a2ad161dc2dcf0d63ad6a12bd
Austin Widner <austinw125@gmail.com>
2013-07-14 19:33:14 -0700

    Revert "mm-core: Add key for HFR encoding"
    This reverts commit a983eede4af380321c411d3cf1b95c7c446113b1.

--- mm-core/inc/QCMetaData.h
+++ mm-core/inc/QCMetaData.h
@@ -61,7 +61,6 @@ enum {
     //Extractor sets this
     kKeyUseArbitraryMode     = 'ArbM',  //bool (int32_t)
     kKeySmoothStreaming      = 'ESmS',  //bool (int32_t)
-    kKeyHFR                  = 'hfr ',  // int32_t
 };
 
 enum {
